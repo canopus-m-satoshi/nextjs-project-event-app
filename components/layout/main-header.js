6442b496b2d38be1527f90aa7d94d8918bf487e5
Satoshi Matsushima <>
2021-12-27 00:07:11 +0900

    77. Adding a General Layout Wrapper Component

---
+++ components/layout/main-header.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+import classes from './main-header.module.css';
+
+function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">
+          {/* <Logo /> */}
+          NextEvents
+        </Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">Brous All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainHeader;
