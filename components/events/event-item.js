339a169cb763a90088f7c8fdd9591992ecfd3dea
Satoshi Matsushima <>
2021-12-26 22:10:30 +0900

    73. Adding More React Components & Connecting...

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import Link from 'next/link';
 
 function EventItem(props) {
-  return <li></li>;
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const formattedAddress = location.replace(',', '\n');
+
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li>
+      <img src={'/' + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Expolore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
